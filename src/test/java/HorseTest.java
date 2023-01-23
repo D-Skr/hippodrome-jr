859a811691b47cee56865b5d3b8cca2c88d72997
Dmitriy <altusj@gmail.com>
2023-01-23 16:42:45 -0500

    add test for main method

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class HorseTest {
+public class HorseTest {
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainTest {
+
+    @Test
+    @Timeout(value = 22, unit = TimeUnit.SECONDS)
+    void failIfExecutionTimeExceeds22Seconds() throws Exception{
+        Main.main(null);
+    }
+
 }
