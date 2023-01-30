530733dffcf2b02cfacfdc5721a2465c12927b02
Dmitriy <altusj@gmail.com>
2023-01-29 19:38:00 -0500

    new tests for Horse constructor

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,25 +1,71 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HorseTest {
 
+    private String horseName = "Klyacha";
+    private Double horseSpeed = 1.1;
     @Test
     //@ParameterizedTest
     @NullSource
 
-    void whenHorseNameNull(){
+    void constructorHorseNameNull(){
         //Check Horse constructor: name cannot be null
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () ->
                     //throw new IllegalArgumentException("Name cannot be null.");
-                    new Horse(null, 0.1));
+                    new Horse(null, horseSpeed));
 
+                //Check the message if name is null
                 assertEquals("Name cannot be null.", exception.getMessage());
-        System.out.println(exception.getMessage());
+        //System.out.println(exception.getMessage());
+    }
+
+    //Check for empty strings and only-spaces name
+    @ParameterizedTest
+    @ValueSource(strings = {""," ","\t","\s"})
+        void constructorHorseNameEmptyString(String emptyName) {
+            IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        //throw new IllegalArgumentException("Name cannot be null.");
+                        new Horse(emptyName, horseSpeed));
+
+        //Check the message if name is blank
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
+
+    //Check for negative double in speed
+    @ParameterizedTest
+    @ValueSource(doubles = {-500,-1.5,-0.1})
+    void constructorHorseSpeedNegativeNumber(Double doubles) {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        //throw new IllegalArgumentException("Speed cannot be negative.");
+                        new Horse(horseName, doubles));
+
+        //Check the message if speed is negative
+        assertEquals("Speed cannot be negative.", exception.getMessage());
+    }
+
+    //Check for negative number in distance
+    @ParameterizedTest
+    @ValueSource(doubles = {-500,-1.5,-0.1})
+    void constructorHorseDistanceNegativeNumber(Double doubles) {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        //throw new IllegalArgumentException("Distance cannot be negative.");
+                        new Horse(horseName, horseSpeed, doubles));
+
+        //Check the message if distance is negative
+        assertEquals("Distance cannot be negative.", exception.getMessage());
     }
 }
